1f59e54a17668165b39a35eca92dada2072654e0
AnilYadav143 <samyaduvanshi26@gmail.com>
2023-06-18 11:45:44 +0530

    added institute images

--- resources/views/admin/my_certificate.blade.php
+++ resources/views/admin/my_certificate.blade.php
@@ -12,8 +12,8 @@
                 @if(isset($crtfct->certificate))
                     @php
                         $extname = explode('.',$crtfct->certificate);
-                        $extn   =   end($crtfct);
-                        var_dump($crtfct);
+                        $extn   =   end($extname);
+                       
                     @endphp
                     @if($extn == 'pdf')
                     <object data="{{url('admin/certificate/'.$crtfct->certificate)}}" type="application/pdf" width="100%" height="500px">
